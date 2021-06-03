435cc3cb53dacc0df4a65ab750f37bd0037dd8e0
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-03 18:18:19 -0500

    utilize implemtnation for all methods

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::{env, fs, string::String};
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let config = parse_config(&args);
-	let contents = read_file(&config.filename);
+	let config = Config::new(&args);
+	let contents = config.read();
 	println!("Searching for '{}' in file '{}'", config.query, config.filename);
 	println!("\nFile contains the following text:\n\n{}", contents);
 }
@@ -13,13 +13,15 @@ struct Config {
 	filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-	let query = args[1].clone();
-	let filename = args[2].clone();
-	Config { query, filename }
-}
+impl Config {
+	fn new(args: &[String]) -> Config {
+		let query = args[1].clone();
+		let filename = args[2].clone();
+		Config { query, filename }
+	}
 
-fn read_file(filename: &String) -> String {
-	fs::read_to_string(filename)
-		.expect("Something went wrong when reading the file")
+	fn read(&self) -> String {
+		fs::read_to_string(&self.filename)
+			.expect("Something went wrong when reading the file")
+	}
 }
