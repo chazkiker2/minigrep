446bd9da4dbfa9983842d3bd8d61895d901c7e1e
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-03 17:50:29 -0500

    accept args; read file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-}
+	let query = &args[1];
+	let filename = &args[2];
 
+	let contents = fs::read_to_string(filename)
+		.expect("Something went wrong reading the file");
+
+	println!("Searching for '{}' in file '{}'", query, filename);
+
+	println!("File contains the following text:\n{}", contents);
+}
