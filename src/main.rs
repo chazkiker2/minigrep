f4da732e2202cbb636baf036aff0133afb189381
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-03 18:12:12 -0500

    modularize functions; update to utilize 'Config' struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use std::{env, fs};
+use std::{env, fs, string::String};
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let query = &args[1];
-	let filename = &args[2];
+	let config = parse_config(&args);
+	let contents = read_file(&config.filename);
+	println!("Searching for '{}' in file '{}'", config.query, config.filename);
+	println!("\nFile contains the following text:\n\n{}", contents);
+}
 
-	let contents = fs::read_to_string(filename)
-		.expect("Something went wrong reading the file");
+struct Config {
+	query: String,
+	filename: String,
+}
 
-	println!("Searching for '{}' in file '{}'", query, filename);
+fn parse_config(args: &[String]) -> Config {
+	let query = args[1].clone();
+	let filename = args[2].clone();
+	Config { query, filename }
+}
 
-	println!("File contains the following text:\n{}", contents);
+fn read_file(filename: &String) -> String {
+	fs::read_to_string(filename)
+		.expect("Something went wrong when reading the file")
 }
