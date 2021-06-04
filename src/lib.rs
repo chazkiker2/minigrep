1bbe2bf3cacdfce355a2963ac29d57d20adb30f3
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-04 10:45:00 -0500

    add case insensitive feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-use std::{error::Error, fs, result::Result, string::String};
+use std::{
+	env,
+	error::Error,
+	fs,
+	result::Result,
+	string::String,
+};
 
 /// a fully valid minigrep command
 ///
@@ -8,27 +14,24 @@ pub struct Config {
 	pub query: String,
 	/// the file to search within
 	pub filename: String,
+	pub case_sensitive: bool,
 }
 
 
 impl Config {
 	/// attempt to create a new config from given arguments
-	///
-	/// Example:
-	/// ```rust
-	/// let query = String::from("search query");
-	/// let filename = String::from("filename.txt");
-	///
-	/// let config = Config::new(&vec![query, filename]).unwrap();
-	/// ```
 	pub fn new(args: &[String]) -> Result<Config, &str> {
 		if args.len() < 3 {
-			Err("not enough arguments")
-		} else {
-			let query = args[1].clone();
-			let filename = args[2].clone();
-			Ok(Config { query, filename })
+			return Err("not enough arguments");
 		}
+
+		let query = args[1].clone();
+		let filename = args[2].clone();
+
+		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+		Ok(Config { query, filename, case_sensitive })
+
 	}
 }
 
@@ -45,13 +48,31 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 	results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+	let query = query.to_lowercase();
+	let mut results = Vec::new();
+	for line in contents.lines() {
+		if line.to_lowercase().contains(&query) {
+			results.push(line);
+		}
+	}
+
+	results
+}
+
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 	let contents = fs::read_to_string(&config.filename)?;
 
+	let results = if config.case_sensitive {
+		search(&config.query, &contents)
+	} else {
+		search_case_insensitive(&config.query, &contents)
+	};
+
 	println!("Searching for '{}' in file '{}'\n", config.query, config.filename);
 
-	for line in search(&config.query, &contents) {
+	for line in results {
 		println!("{}", line);
 	}
 
@@ -66,7 +87,7 @@ mod tests {
 
 	/// test query with no matching lines
 	#[test]
-	fn no_result() {
+	fn case_sensitive_no_result() {
 		let query = "philanthropist";
 		let contents = "no such thing";
 		assert_eq!(vec![] as Vec<&str>, search(query, contents));
@@ -75,7 +96,7 @@ mod tests {
 
 	/// test query with one matching line
 	#[test]
-	fn one_result() {
+	fn case_sensitive_one_result() {
 		let query = "duct"; // expect 'duct' in 'productive'
 		let contents = "\
 Rust:
@@ -85,5 +106,20 @@ Pick three.";
 		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
 	}
 
+	/// test case-insensitive query with matching result
+	#[test]
+	fn case_insensitive_one_result() {
+		let query = "rUsT";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+		assert_eq!(
+			vec!["Rust:", "Trust me."],
+			search_case_insensitive(query, contents)
+		);
+	}
 
 }
