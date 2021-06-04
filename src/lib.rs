096f0f2e1894747933640ff8a746459c1acb5ab8
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-04 10:23:29 -0500

    add whitespace, comments, and general code clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,26 @@
 use std::{error::Error, fs, result::Result, string::String};
 
+/// a fully valid minigrep command
+///
+/// consists of a `query` to search for and a `filename` to search within
 pub struct Config {
+	/// the query to search for
 	pub query: String,
+	/// the file to search within
 	pub filename: String,
 }
 
+
 impl Config {
+	/// attempt to create a new config from given arguments
+	///
+	/// Example:
+	/// ```rust
+	/// let query = String::from("search query");
+	/// let filename = String::from("filename.txt");
+	///
+	/// let config = Config::new(&vec![query, filename]).unwrap();
+	/// ```
 	pub fn new(args: &[String]) -> Result<Config, &str> {
 		if args.len() < 3 {
 			Err("not enough arguments")
@@ -17,6 +32,7 @@ impl Config {
 	}
 }
 
+/// search through each line in given `contents` and return any lines containing a match to the `query`
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 	let mut results = Vec::new();
 
@@ -28,6 +44,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
 	results
 }
+
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 	let contents = fs::read_to_string(&config.filename)?;
@@ -46,6 +63,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 mod tests {
 	use super::*;
 
+
+	/// test query with no matching lines
 	#[test]
 	fn no_result() {
 		let query = "philanthropist";
@@ -53,9 +72,11 @@ mod tests {
 		assert_eq!(vec![] as Vec<&str>, search(query, contents));
 	}
 
+
+	/// test query with one matching line
 	#[test]
 	fn one_result() {
-		let query = "duct";
+		let query = "duct"; // expect 'duct' in 'productive'
 		let contents = "\
 Rust:
 safe, fast, productive.
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@ use std::{env, process};
 
 use minigrep::Config;
 
+
+/// run minigrep CLI application
+///
+/// this is the main entry-point for minigrep
 fn main() {
+	// command line arguments input by user
 	let args: Vec<String> = env::args().collect();
+
+	// try to parse arguments into minigrep config; convey error and exit if fail
 	let config = Config::new(&args).unwrap_or_else(|err| {
 		println!("Problem parsing arguments: {}", err);
 		process::exit(1);
 	});
+
+	// try to run minigrep; convey error and exit if fail
 	if let Err(e) = minigrep::run(config) {
 		println!("Application error: {}", e);
 		process::exit(1);
